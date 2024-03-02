3754317444a90a2e3d46381ca1ebe4e88c7b2f46
Pankaj-2003 <pankajtiwaribs@gmail.com>
2024-03-02 18:49:24 +0530

    set up project structure and wrote exception , setup and tested them

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+from logger import logging
+
+from exception import CustomException
+
+
+try:
+    pass
+
+except Exception as e:
+    raise CustomException(e,sys)
